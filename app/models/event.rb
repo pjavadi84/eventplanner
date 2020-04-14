017b776d056e33a82c6730df4a50f6c6e4b3216b
github username <github email address>
2020-04-14 12:49:53 -0700

    adding booking model and setting up new associations. booking table has not being set up yet

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+    belongs_to :user
+    belongs_to :property
+    has_many :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :events
     has_many :properties, through: :events
+    has_many :bookings
     has_secure_password
 end
