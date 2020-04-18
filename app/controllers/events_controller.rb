9b5f004e4e6b3e0b3be2b212df4979db6d9bab6d
github username <github email address>
2020-04-18 13:48:34 -0700

    create logic to create, edit, and delete an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,19 +12,43 @@ class EventsController < ApplicationController
     end
 
     def create 
+        @event = current_property.events.build(event_params)
+        if @event.save
+            redirect_to property_events_path(@event)
+        else
+            render :new
+        end
+    end
 
+    def show
+        @event = Event.find(params[:id])
     end
 
     def edit
-
+        if params[:property_id]
+            property = Property.find_by(id: params[:property_id])
+            if property.nil?
+                redirect_to properties_path, alert: "property not found"
+            else
+                @event = property.events.find_by(id: params[:id])
+                redirect_to property_events_path(property), alert: "Event not found." if @event.nil?
+            end
+        else
+            @event = Event.find(params[:id])
+        end
     end
 
     def update 
-
+        @event = Event.find(params[:id])
+        @event.update(event_params)
+        redirect_to property_event_path(@event)
     end
 
     def destroy
-
+        @event = Event.find(params[:id])
+        @event.destroy
+        flash[:notice] = "Event deleted"
+        redirect_to property_events_path(@event)
     end
 
     private 
@@ -34,7 +58,6 @@ class EventsController < ApplicationController
             :event_name,
             :ticket_price,
             :minimum_age,
-            :event_type,
             :number_of_guests,
             :event_date
         )
